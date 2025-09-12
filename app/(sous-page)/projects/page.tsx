6a4d559439bea4c3dba0ce5fcf4ab93636ca7d54
LionotKennedy <razafimandimbylionotkennedy2gmail.com>
2025-09-12 13:59:47 +0300

    clear project page bro

--- app/(sous-page)/projects/page.tsx
+++ app/(sous-page)/projects/page.tsx
@@ -1,12 +1,8 @@
-import Image from "next/image";
-import bg from "../../../public/background/projects-background.png";
 import type { JSX } from "react/jsx-runtime";
 import { Metadata } from "next";
 import ProjectList from "../../components/projects/index";
 import { projectsData } from "../../data/data";
 import BackgroundHome from "@/app/components/BackgroundHome";
-// import RenderModel from "@/app/components/RenderModel";
-// import Staff from "@/app/components/models/Staff";
 
 export const metadata: Metadata = {
   title: "Projects",
@@ -15,21 +11,10 @@ export const metadata: Metadata = {
 export default function Home(): JSX.Element {
   return (
     <>
-      {/* <Image
-        priority
-        sizes="100vw"
-        src={bg || "/placeholder.svg"}
-        alt="background-image"
-        // className="-z-50 w-full h-full fixed object-cover object-center opacity-50"
-        className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-50"
-      /> */}
       <BackgroundHome />
-
       <ProjectList projects={projectsData} />
       <div className="flex items-center justify-center fixed top-16 lg:top-20 -translate-x-1/2 lg:translate-x-0 -z-10 left-1/2 lg:-left-24 h-screen">
-        {/* <RenderModel>
-          <Staff />
-        </RenderModel> */}
+
       </div>
     </>
   );
