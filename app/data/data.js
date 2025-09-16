412900584ce039db3642485e0c183f475e4ca73f
LionotKennedy <razafimandimbylionotkennedy2gmail.com>
2025-09-16 14:55:21 +0300

    add link for github, linkedin and twitter

--- app/data/data.js
+++ app/data/data.js
@@ -85,19 +85,19 @@ export const BtnList = [
   { label: "Contact", link: "/contact", icon: "contact", newTab: false },
   {
     label: "Github",
-    link: "https://www.github.com/codebucks27",
+    link: "https://github.com/LionotKennedy",
     icon: "github",
     newTab: true,
   },
   {
     label: "LinkedIn",
-    link: "https://www.linkedin.com/in/codebucks",
+    link: "https://www.linkedin.com/in/lionot-razafimandimby-244073266/",
     icon: "linkedin",
     newTab: true,
   },
   {
     label: "X",
-    link: "https://www.x.com/code_bucks",
+    link: "https://x.com/RLionot",
     icon: "twitter",
     newTab: true,
   },
